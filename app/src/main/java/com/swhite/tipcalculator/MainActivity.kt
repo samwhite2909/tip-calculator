3052b27ec0f64e03d61f7307d4ee0c5c0933603d
samwhite2909 <Bulldopzer98>
2023-03-07 15:49:01 +0000

    Adding in the text input field and the buttons for adding more people.

--- app/src/main/java/com/swhite/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/swhite/tipcalculator/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.swhite.tipcalculator
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -8,26 +10,37 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.swhite.tipcalculator.components.InputField
 import com.swhite.tipcalculator.ui.theme.TipCalculatorTheme
+import com.swhite.tipcalculator.widgets.RoundIconButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Application {
-                TopHeader()
+                //TopHeader()
+                MainContent()
             }
         }
     }
@@ -75,9 +88,30 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun MainContent() {
+    BillForm() { billAmount ->
+        Log.d("TEST123", billAmount.toInt().toString())
+
+    }
+
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(
+    modifier: Modifier = Modifier,
+    onValChange: (String) -> Unit = {}
+) {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
     Surface(
         modifier = Modifier
             .padding(2.dp)
@@ -85,10 +119,59 @@ fun MainContent() {
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
-        Column() {
-            
-        }
+        Column(
+            modifier = Modifier.padding(6.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    onValChange(totalBillState.value.trim())
 
+                    keyboardController?.hide()
+                })
+            if (validState) {
+                Row(
+                    modifier = Modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = "Split",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                    )
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row(
+                        modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        RoundIconButton(
+                            imageVector = Icons.Default.Remove,
+                            onClick = { }
+                        )
+                        Text(
+                            text = "2",
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(
+                                    start = 9.dp,
+                                    end = 9.dp
+                                )
+                        )
+                        RoundIconButton(
+                            imageVector = Icons.Default.Add,
+                            onClick = { }
+                        )
+                    }
+                }
+            } else {
+                Box() {}
+            }
+        }
     }
 }
 
@@ -98,6 +181,7 @@ fun DefaultPreview() {
     TipCalculatorTheme {
         Application {
             TopHeader()
+            MainContent()
         }
     }
 }
